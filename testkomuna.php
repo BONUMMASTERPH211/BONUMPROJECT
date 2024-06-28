6d593df8dafa6f9ce24c6338d8ee6288558eafd7
BONUMMASTERPH211 <81369695+BONUMMASTERPH211@users.noreply.github.com>
2024-06-28 14:53:36 +0800

    Update testkomuna.php

--- testkomuna.php
+++ testkomuna.php
@@ -2,7 +2,8 @@
 
 function telegram($msg) {
         global $telegrambot,$telegramchatid;
-        $url = 'https://api.telegram.org/bot'.$telegrambot.'/sendMessage';$data = array('chat_id'=>$telegramchatid,'text'=>$msg);
+        $url = 'https://api.telegram.org/bot'.$telegrambot.'/sendMessage';$data = array('chat_id'=>
+$telegramchatid,'text'=>$msg);
         $options = array('http'=>array('method' => 'POST','header' => "Content-Type:application/x-www-form-urlencoded\r\n",'content' => http_build_query($data),),);
         $context = stream_context_create($options);
         $result = file_get_contents($url,false,$context);
@@ -27,4 +28,5 @@ telegram("
     GOODVIBES :)
   </h1>  
 </html>
+
 
